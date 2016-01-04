ca5da2f337db34d9f836bc1ae1b6d30200fd3823
suboy <suboy13@163.com>
2016-01-04 10:05:42 +0800

    include init
    new version

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-﻿var path = require('path');
+var path = require('path');
 var fs = require('fs');
 
 var global = this;
-var map = {};
+var map = {};	//checkTime, modifyTime
+var checkInterval = 0;
 
-module.exports = function(p){
+// var live = function(p){return require('live')(require.resolve(p))};
+
+module.exports.init = function(option){
+	checkInterval = option.interval*1000||0;
+}
+
+module.exports.liveRequire = function(p){
 	var modulePath = require.resolve(p);
+	//var d = Date.now();
 	//console.log(modulePath)
 	if(needReload(modulePath)){
 		console.log('load: '+modulePath);
 		delete require.cache[modulePath];
 	}
+	//console.log(Date.now()-d);
 	return require.apply(global, [p]);
 }
 
-function needReload(modulePath){
-	if(!fs.existsSync(modulePath)){
-		console.log('no module file: '+modulePath)
+function needReload(mp){
+	if(!fs.existsSync(mp)){
+		console.log('no module file: '+mp)
 		return false;
 	}
-	var stat = fs.statSync(modulePath);
-	var lastTime = map[modulePath]||0;
+	var stat = fs.statSync(mp);
+	if(!stat) return false;
+	var moduleInfo = map[mp] = map[mp]||{};
+	if(moduleInfo.checkTime+checkInterval>Date.now()) return false;
+	//console.log('date: '+(moduleInfo.checkTime+checkInterval-Date.now()));
+	moduleInfo.checkTime = Date.now();
+	var lastTime = moduleInfo.modifyTime||0;
 	var modifyTime = stat.mtime.getTime();
-	if(stat&&lastTime!=modifyTime){
-		map[modulePath] = modifyTime;
+	if(lastTime!=modifyTime){
+		moduleInfo.modifyTime = modifyTime;
 		//console.log(modifyTime);
 		return true;
 	}else{
